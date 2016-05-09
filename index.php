bb4186af69b06d3262f185f366988a42bc1bf69f
mayingbing <mayingbing@163.com>
2016-05-09 12:50:07 +0800

    2016.5.9

--- index.php
+++ index.php
@@ -1,28 +1,15 @@
 <?php
-//入口文件
-//引入核心启动类
+/**
+ * Created by PhpStorm.
+ * User: mayingbing
+ * Date: 16/5/9
+ * Time: 11:06
+ * 入口文件
+ * 引入核心启动类
+ */
+
 include "framework/core/Framework.class.php";
-// $app = new Framework();
-// $app->run();
-#Framework::run();
-// echo getcwd();
-$link = mysql_connect("localhost","root","ma151626");
+//$app = new FrameworkClass();
+//$app->run();
+Framework::run();
 
-mysql_query("set names utf8");
-
-mysql_query("use ma_shop_db");
-
-mysql_query("insert into matab(f1,f2) value(123,123);");
-
-
-
-
-
-
-
-
-
-
-#mysqli_close();
-#echo"connect successful close";
-?>

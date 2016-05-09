1c0d1ce832146b6e8f6c20247f922fa15a64a59d
mayingbing <mayingbing@163.com>
2016-05-09 16:25:02 +0800

    start

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mayingbing
- * Date: 16/5/9
- * Time: 11:06
- * 入口文件
- * 引入核心启动类
- */
-
+//入口文件
+//引入核心启动类
 include "framework/core/Framework.class.php";
-//$app = new FrameworkClass();
-//$app->run();
+// $app = new Framework();
+// $app->run();
 Framework::run();
+// echo getcwd();
+
 
